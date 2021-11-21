5307a789c9039df8d72f0dda18e38b1267486637
algnot <thanawat.k2000@gmail.com>
2021-11-21 16:47:36 +0700

    back to basic react!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import './global.css';
 
 function App() {
   return (
     <div className="App">
-     benz na rak is true yeye
+      
     </div>
   );
 }
